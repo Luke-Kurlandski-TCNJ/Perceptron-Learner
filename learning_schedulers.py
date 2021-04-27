1cda494e7dbe594e435b3214649e9ecced37e42c
Luke Kurlandski <kurlanl1@dev001.cm.cluster>
2021-04-27 17:55:09 -0400

    updated comment

--- learning_schedulers.py
+++ learning_schedulers.py
@@ -12,6 +12,10 @@ def exponential_decay(epoch, lr_initital=.01, base=.1, n_steps=20):
 		lr_initial : float : the initial learning rate used
 		base : float : base for the exponential decay
 		n_steps : int : slash learning rate by base times every n_steps
+
+	Returns
+	--------------------------------------------------------------------
+		float : the adjusted learning rate
 	"""
 
 	return lr_initital * base**(epoch / n_steps)
