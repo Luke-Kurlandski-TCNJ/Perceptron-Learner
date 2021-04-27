72fc7acfb45b6ab032971d1acf86c45adcecfd2c
Luke Kurlandski <kurlanl1@dev001.cm.cluster>
2021-04-27 17:51:24 -0400

    added a learning scheduler for learning rate if we choose to use it

---
+++ learning_schedulers.py
@@ -0,0 +1,17 @@
+"""
+Learning scheduling algorithms to be used in the perceptron learning.
+"""
+
+def exponential_decay(epoch, lr_initital=.01, base=.1, n_steps=20):
+	"""
+	Exponential learning rate decay.
+
+	Arguments
+	--------------------------------------------------------------------
+		epoch : int : the current training epoch
+		lr_initial : float : the initial learning rate used
+		base : float : base for the exponential decay
+		n_steps : int : slash learning rate by base times every n_steps
+	"""
+
+	return lr_initital * base**(epoch / n_steps)
