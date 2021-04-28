e101622de9d66aa1fa9d4f699d69ea4417c04f5b
Luke Kurlandski <kurlanl1@dev001.cm.cluster>
2021-04-28 12:45:17 -0400

    updated plotting to plot all curves on same plot; also included empty files for the submissions; also updated the README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Perceptron-Learner
 A basic perceptron learner configured to classify data from the iris dataset.
+
+# Deliverables
+- D2.csv : epoch stats file for T2
+- D2.png : plot for each of the three LPs for T2
+- D3.csv : epoch stats file for T3
+- D3.png : plot for each of the three LPs for T3
+- D4.csv : epoch stats file for T4
+- D4.png : plot for each of the three LPs for T4
+- D5.pdf : report for T5
+- D6.pdf : writeup and reflection
--- plotting.py
+++ plotting.py
@@ -19,29 +19,49 @@ import sys
 
 import pandas as pd
 
-def errors_vs_epochs(input_path, output_path):
+def errors_vs_epochs(in1, in2, in3, output_path):
 	"""
 	Plot the number of training errors vs the number of epochs.
 
 	Arguments
 	--------------------------------------------------------------------
-		input_path : str : data collected from perceptron stored in a 
-			.csv file with columns: ['epoch','errors', 'weights']
+		in_1 : str : .csv file for learning problem 1
+		in_2 : str : .csv file for learning problem 2
+		in_3 : str : .csv file for learning problem 3
 		output_path : str : .png image file to save the plot to
+
+	Notes
+	--------------------------------------------------------------------
+		The csv files should have columns: 'epoch','errors','weights'
 	"""
 
-	df = pd.read_csv(input_path, usecols=['epoch','errors'])
-	ax = df.plot.line(x='epoch', y='errors')
+	df1 = pd.read_csv(in1, usecols=['epoch','errors'])
+	df2 = pd.read_csv(in2, usecols=['epoch','errors'])
+	df3 = pd.read_csv(in3, usecols=['epoch','errors'])
+
+	df1 = df1.rename(columns={"errors": "errors LP 1"})
+	df2 = df2.rename(columns={"errors": "errors LP 2"})
+	df3 = df3.rename(columns={"errors": "errors LP 3"})
+
+	df = pd.merge(pd.merge(df1, df2, on='epoch'), df3, on='epoch')
+
+	print(df)
+
+	ax = df.plot.line(x='epoch', y=['errors LP 1','errors LP 2','errors LP 3'])
 	ax.set_xlabel("epoch")
 	ax.set_ylabel("# errors")
 	ax.figure.savefig('sample_plot.png')
 
 def main():
-	if len(sys.argv) < 3:
-		print("Read plotting.py, specify input and output.")
-	input_path = sys.argv[1]
-	output_path = sys.argv[2]
-	errors_vs_epochs(input_path, output_path)
+	if len(sys.argv) < 4:
+		print("Read plotting.py, specify input and output paths.")
+
+	in1 = sys.argv[1]
+	in2 = sys.argv[2]
+	in3 = sys.argv[3]
+	output_path = sys.argv[4]
+
+	errors_vs_epochs(in1, in2, in3, output_path)
 
 if __name__ == "__main__":
 	main()
