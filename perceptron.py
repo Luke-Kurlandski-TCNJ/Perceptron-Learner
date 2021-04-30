7bb4dbc5943ef4c414a67f8a538e180ea5a78853
adeena210 <57496912+adeena210@users.noreply.github.com>
2021-04-30 06:11:04 +0400

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -5,6 +5,7 @@ Notes
 ------------------------------------------------------------------------
 	What should the default weights of the perceptron be?
 """
+import csv 
 
 class Perceptron:
 	"""
@@ -23,20 +24,24 @@ class Perceptron:
 	--------------------------------------------------------------------
 	"""
 
-	def __init__(self, weights=None):
+	def __init__(self, name, weights=None):
 		"""
 		Handle the initialization of the weights for the perceptron.
 		"""
+		self.targetClass = name
+		self.lr = 0.1
+		if weights is not None:
+			self.weights = weights
+		else:
+			weights = [0,0,0,0,0]
 
-		pass
-
-	def fit(self, data, path):
+	def fit(self, data, path = None):
 		"""
 		Learn the data by updating the perceptron's weights.
 
 		Arguments
 		----------------------------------------------------------------
-			data : {} : a dictionary of iris data with following keys:
+			data : list : list of dictionaries containing training instances with the following keys:
 				'sepal_length', 
 				'sepal_width', 
 				'petal_length', 
@@ -49,8 +54,34 @@ class Perceptron:
 			Should output a csv file with column headers for the epoch,
 				number of errors, etc. as described in requirements
 		"""
-
-		pass
+		epoch = 0
+		error = -1
+		name = self.targetClass + 'stats.csv'
+		if path is None:
+			file = open(name, 'a', newline='')
+		else:
+			file = open(path+"/"+name, 'a', newline='')
+		write = csv.writer(file)
+		write.writerow(["epoch", "error", "weights"])
+		
+		while error != 0:
+			epoch += 1
+			error = 0
+			for i in range(len(data)):
+				target = 1 if (self.targetClass == data[i]['class']) else -1
+				x = [1]
+				for key in data[i].keys:
+					if key != "class":
+						x.append(data[i][key])
+				diff = target - self.output(x)
+				if diff == 0:
+					continue
+				for i in range(len(self.weights)):
+					error += 1
+					delta = self.lr * diff * x[i]
+					self.weights[i] = self.weights[i] + delta 
+			write.writerow([epoch, error, self.weights])	
+			 
 
 	def output(self, x):
 		"""
@@ -64,7 +95,12 @@ class Perceptron:
 		----------------------------------------------------------------
 			z : int : sgn(<x, self.weights>), i.e., +1 or -1
 		"""
-
-		pass
-
+		for i in range(self.weights):
+			sum += self.weights[i] * x[i]
 		
+		z = 1 if (sum > 0) else 0
+	
+		return z
+
+	
+	
