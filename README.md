9943e3fc9d5f2a65f5d717abb6285f57023425c5
Luke Kurlandski <kurlanl1@dev001.cm.cluster>
2021-04-30 17:37:20 -0400

    setup learning problems, added csv file placeholders, updated README

--- README.md
+++ README.md
@@ -2,11 +2,17 @@
 A basic perceptron learner configured to classify data from the iris dataset.
 
 # Deliverables
-- D2.csv : epoch stats file for T2
+- D2_iris_setosa_stats.csv : epoch stats file for task 2, learning problem 1
+- D2_iris_versicolor_stats.csv : epoch stats file for task 2, learning problem 2
+- D2_iris_virginica_stats.csv : epoch stats file for task 2, learning problem 3
 - D2.png : plot for each of the three LPs for T2
-- D3.csv : epoch stats file for T3
+- D3_iris_setosa_stats.csv : epoch stats file for task 2, learning problem 1
+- D3_iris_versicolor_stats.csv : epoch stats file for task 2, learning problem 2
+- D3_iris_virginica_stats.csv : epoch stats file for task 2, learning problem 3
 - D3.png : plot for each of the three LPs for T3
-- D4.csv : epoch stats file for T4
+- D4_iris_setosa_stats.csv : epoch stats file for task 2, learning problem 1
+- D4_iris_versicolor_stats.csv : epoch stats file for task 2, learning problem 2
+- D4_iris_virginica_stats.csv : epoch stats file for task 2, learning problem 3
 - D4.png : plot for each of the three LPs for T4
 - D5.pdf : report for T5
 - D6.pdf : writeup and reflection
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Run the perceptron learning tasks and the learning problems.
 from perceptron import Perceptron
 from utils import process_iris_data
 
-def learning_problems(weights, data):
+def learning_problems(weights, data, task):
 	"""
 	Acomplish the three learning tasks as outlined in Task 2.
 
@@ -17,13 +17,29 @@ def learning_problems(weights, data):
 	
 	Arguments
 	--------------------------------------------------------------------
-		weights : [] : list of weights to use for learning problems 
-			(different learning problems specify different weights)
-		data : {} : the iris data to use for learning problems
-			(different learning problems specify different shufflings)
+		weights : [] : list of weights to use for learning problems
+		data : [{}] : a list of iris data, each element is an individual
+			example implemented as a dictionary with the following keys:
+			'sepal_length', 
+			'sepal_width', 
+			'petal_length', 
+			'petal_width', 
+			'class'
+		task : int : the task number that this function fulfils
+
+	Notes
+	--------------------------------------------------------------------
+		Will produce three csv files containing the stats for this task.
 	"""
 
-	pass
+	p = Perceptron(name='iris_setosa', weights=weights)
+	p.fit(data=data, path=f'D{task}_iris_setosa_stats.csv')
+
+	p = Perceptron(name='iris_versicolor', weights=weights)
+	p.fit(data=data, path=f'D{task}_iris_versicolor_stats.csv')
+
+	p = Perceptron(name='iris_virginica', weights=weights)
+	p.fit(data=data, path=f'D{task}_iris_virginica_stats.csv')
 
 def task4():
 	"""
