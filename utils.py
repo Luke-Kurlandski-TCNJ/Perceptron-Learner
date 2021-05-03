bc16a15817cb42edb3ca793ff38f572bf28f41f5
adeena210 <57496912+adeena210@users.noreply.github.com>
2021-05-03 22:56:08 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,18 +1,21 @@
 """
-Useful functions.
+Useful functions for data extraction.
 """
 
-def process_iris_data(path='iris.data'):
+import pprint
+import csv
+
+def process_iris_data(path='iris.data', verbose=False):
 	"""
 	Process the iris data from file and return python object.
-
 	Arguments
 	--------------------------------------------------------------------
 		path : str : location of comma separated iris data
-
+		verbose : bool : if True, prints out the data prior to returning
 	Returns
 	--------------------------------------------------------------------
-		data : {} : a dictionary of iris data with the following keys:
+		data : [{}] : a list of iris data, each element is an individual
+			example implemented as a dictionary with the following keys:
 			'sepal_length', 
 			'sepal_width', 
 			'petal_length', 
@@ -20,4 +23,19 @@ def process_iris_data(path='iris.data'):
 			'class'
 	"""
 
-	pass
+	fieldnames = [
+		'sepal_length', 
+		'sepal_width', 
+		'petal_length', 
+		'petal_width', 
+		'class'
+	]
+
+	with open(path, newline='') as csvfile:
+		reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+		data = list(map(lambda x: dict(x), reader))
+
+	if verbose:
+		pprint.pprint(data)
+
+	return data
